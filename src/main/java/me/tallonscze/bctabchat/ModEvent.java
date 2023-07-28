ec7dbe04d9858f2fa44e21c392fba1f9f64f4cc6
TallonsCZ <tallons@burningcube.eu>
2023-07-28 12:58:26 +0200

    Adds NameFormat, Needs LuckPerms client and server side

--- src/main/java/me/tallonscze/bctabchat/ModEvent.java
+++ src/main/java/me/tallonscze/bctabchat/ModEvent.java
@@ -1,12 +1,8 @@
 package me.tallonscze.bctabchat;
 
-import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.event.LuckPermsEvent;
 import net.luckperms.api.model.user.User;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,36 +11,12 @@ public class ModEvent {
 
     @Mod.EventBusSubscriber(modid = Bctabchat.MODID)
     public static class ForgeEvents{
+
         @SubscribeEvent
         public static void onTabListNameFormat(PlayerEvent.TabListNameFormat event){
             Component playerName = event.getEntity().getDisplayName();
-            String uuid = event.getEntity().getStringUUID();
             String sPlayerName = playerName.getString();
             User user = LuckPermsProvider.get().getUserManager().getUser(sPlayerName);
-            String lPrefix;
-            if (user != null){
-                lPrefix = user.getCachedData().getMetaData().getPrefix();
-                if (lPrefix != null){
-                    if (lPrefix.contains("&")){
-                        lPrefix = lPrefix.replace("&", "§");
-                    }else{
-                        return;
-                    }
-                }
-            } else {
-                lPrefix = "§4 Not Working";
-            }
-            Component prefix = Component.literal(lPrefix + " " + sPlayerName);
-            event.setDisplayName(prefix);
-            event.getEntity().refreshDisplayName();
-
-        }
-
-        @SubscribeEvent
-        public static void onChatNameFormat(PlayerEvent.NameFormat event){
-            Component playerName = event.getEntity().getDisplayName();
-            String stringPlayerName = playerName.getString();
-            User user = LuckPermsProvider.get().getUserManager().getUser(stringPlayerName);
             String lPrefix = "";
             if (user != null){
                 lPrefix = user.getCachedData().getMetaData().getPrefix();
@@ -52,8 +24,32 @@ public class ModEvent {
             if (lPrefix != null && lPrefix.contains("&")){
                 lPrefix = lPrefix.replace("&", "§");
             }
-            Component finish = Component.literal(lPrefix + " " + stringPlayerName);
-            event.setDisplayname(finish);
+            Component prefix = Component.literal(lPrefix + " " + sPlayerName);
+            event.setDisplayName(prefix);
+            event.getEntity().refreshDisplayName();
+
+        }
+
+
+        @SubscribeEvent
+        public static void onChatNameFormat(PlayerEvent.NameFormat event){
+            try {
+                Component playerName = event.getEntity().getName();
+                String stringPlayerName = playerName.getString();
+                User user = LuckPermsProvider.get().getUserManager().getUser(stringPlayerName);
+                String lPrefix = " ";
+                if (user != null){
+                    lPrefix = user.getCachedData().getMetaData().getPrefix();
+                }
+                if (lPrefix != null && lPrefix.contains("&")){
+                    lPrefix = lPrefix.replace("&", "§");
+                }
+                Component finish = Component.literal(lPrefix + " " + stringPlayerName);
+                event.setDisplayname(finish);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
         }
 
     }
